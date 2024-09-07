b9fa54a6d18719e2ddb158060f7649d73c7f5d83
gerardonastri <gerardonastri06@gmail.com>
2024-09-07 17:35:38 +0200

    exit page transition

--- components/header.tsx
+++ components/header.tsx
@@ -1,25 +1,26 @@
 import Link from 'next/link'
 import { ThemeToggle } from './theme-toggle'
+import TransitionLink from '@/utils/TransitionLink'
 
 export default function Header() {
   return (
     <header className='fixed inset-x-0 top-0 z-50 bg-background/75 py-6 backdrop-blur-sm'>
       <nav className='container flex max-w-3xl items-center justify-between'>
         <div>
-          <Link href='/' className='font-serif text-2xl font-bold'>
+          <TransitionLink href='/' className='font-serif text-2xl font-bold'>
             GN
-          </Link>
+          </TransitionLink>
         </div>
 
         <ul className='flex items-center gap-6 text-sm font-light text-muted-foreground sm:gap-10'>
           <li className='transition-colors hover:text-foreground'>
-            <Link href='/posts'>Posts</Link>
+            <TransitionLink href='/posts'>Posts</TransitionLink>
           </li>
           <li className='transition-colors hover:text-foreground'>
-            <Link href='/projects'>Projects</Link>
+            <TransitionLink href='/projects'>Projects</TransitionLink>
           </li>
           <li className='transition-colors hover:text-foreground'>
-            <Link href='/contact'>Contact</Link>
+            <TransitionLink href='/contact'>Contact</TransitionLink>
           </li>
         </ul>
 
---
+++ utils/TransitionLink.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import Link, {LinkProps} from "next/link"
+import React, { ReactNode } from "react"
+import { useRouter } from "next/navigation"
+
+interface TransitionLinkProps extends LinkProps{
+    children: ReactNode,
+    href: string,
+    className?: string
+}
+
+function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+const TransitionLink = ({children, href, className, ...props}: TransitionLinkProps) => {
+
+    const router = useRouter()
+
+    const handleTransition = async (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        e.preventDefault();
+
+        //run animation
+        const body = document.querySelector("body")
+        body?.classList.add("page-transition")
+        //sleep for some time
+        await sleep(500);
+
+        router.push(href);
+        await sleep(500);
+        body?.classList.remove("page-transition")
+    }
+
+  return (
+    <Link onClick={e => handleTransition(e)} className={className} href={href} {...props}>
+        {children}
+    </Link>
+  )
+}
+
+export default TransitionLink
